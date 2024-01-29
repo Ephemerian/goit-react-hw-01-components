dc49519d996afbd8e8771c941214e86bd0060a42
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2024-01-29 20:52:02 +0200

    full hw + rework compopents logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./Friend/FriendList/FriendList"
+import { Transaction } from "./TransactionHistory/TransactionHistory";
+
+import userInfo from '../data/user.json';
+import data from '../data/data.json'
+import friends from '../data/friends.json'
+import transactions from '../data/transactions.json'
+
+
+export const App = () => { 
+  
+  return (  
+    <>      
+      <Profile user={userInfo} />
+      <Statistics data={data} />     
+      <FriendList friends={friends} />           
+      <Transaction items={transactions} />
+    </>
+      
   );
 };
